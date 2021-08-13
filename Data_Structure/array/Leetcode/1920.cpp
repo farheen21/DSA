e2f30eff366d0408bab72307ebb9c3b5c463fa4c
Farheen khan <farheenfirdous12101999@gmail.com>
2021-08-13 03:39:47 -0700

    1 done

---
+++ Data_Structure/array/Leetcode/1920.cpp
@@ -0,0 +1,30 @@
+
+// Brute Force:- for loop with a[i] = nums[nums[i]];
+
+// optimazation :- Uses modulo and / operation for storing the two number.
+
+class Solution
+{
+public:
+    vector<int> buildArray(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] = nums[i] + (n * (nums[nums[i]] % n));
+        }
+        for (int i = 0; i < n; i++)
+        {
+            nums[i] /= n;
+        }
+        return nums;
+    }
+};
+
+// nt n=nums.size();
+//         vector<int> ans(n);
+//         for(int i=0;i<n;i++){
+//             ans[i]=nums[nums[i]];
+//         }
+//         return ans;
